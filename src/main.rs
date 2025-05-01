2c7bd477d95591bbb800ee4b934877e05857bb13
Marco Edenhofer <edenhofermarco@gmail.com>
2025-05-01 16:12:52 +0200

    implement working solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+mod terminal;
+
+use crate::terminal::{ModuleType, Terminal};
+use anyhow::Result;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+
+fn main() -> Result<()> {
+    let terminal = Terminal::new();
+    let root_name = terminal.determine_root_folder_name()?;
+    let resolver_version = determine_resolver_version();
+    create_root_cargo_toml(&root_name, resolver_version)?;
+    let modules_to_create = terminal.determine_modules_for_workspace()?;
+
+    for module in &modules_to_create {
+        let module_type_flag = match module.module_type {
+            ModuleType::Lib => "--lib",
+            ModuleType::Bin => "--bin",
+        };
+        let p = Path::new(&root_name).join(&module.name);
+        let full_path = p.to_str().expect("invalid module name");
+        Command::new("cargo")
+            .args(&["new", module_type_flag, &full_path])
+            .status()?;
+    }
+    println!("Workspace created: '{}'", root_name);
+    Ok(())
+}
+
+fn create_root_cargo_toml(root_name: &str, resolver_version: i32) -> Result<()> {
+    fs::create_dir(&root_name)?;
+    let path = format!("{root_name}/Cargo.toml");
+    fs::write(
+        &path,
+        format!("[workspace]\nresolver = \"{}\"\n", resolver_version),
+    )?;
+
+    Ok(())
+}
+
+fn determine_resolver_version() -> i32 {
+    let resolver_version = map_rust_version_to_resolver_version();
+    println!("Using resolver version '{resolver_version}'");
+    resolver_version
+}
+fn map_rust_version_to_resolver_version() -> i32 {
+    let pre_2021 = "1.55.0";
+    let v2021 = "1.56.0";
+    let v2024 = "1.85.0";
+
+    let is_2024 = version_check::is_min_version(v2024).unwrap_or_else(|| false);
+    let is_2021 = version_check::is_min_version(v2021).unwrap_or_else(|| false);
+    let is_pre_2021 = version_check::is_min_version(pre_2021).unwrap_or_else(|| false);
+
+    if is_2024 || is_2021 {
+        2
+    } else if is_pre_2021 {
+        1
+    } else {
+        println!("Could not determine Rust version -  defaulting resolver to '2'");
+        2
+    }
 }
---
+++ src/terminal.rs
@@ -0,0 +1,71 @@
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::{Confirm, Input, Select};
+use std::env;
+
+pub struct Terminal {
+    theme: ColorfulTheme,
+}
+
+pub enum ModuleType {
+    Lib,
+    Bin,
+}
+
+pub struct ModuleToCreate {
+    pub name: String,
+    pub module_type: ModuleType,
+}
+
+impl Terminal {
+    pub fn new() -> Self {
+        Self {
+            theme: ColorfulTheme::default(),
+        }
+    }
+
+    pub fn determine_root_folder_name(&self) -> dialoguer::Result<String> {
+        let args: Vec<String> = env::args().collect();
+        let root_name = if args.len() > 1 {
+            args[1].clone()
+        } else {
+            Input::with_theme(&self.theme)
+                .with_prompt("Root folder name:")
+                .interact_text()?
+        };
+
+        Ok(root_name)
+    }
+
+    pub fn determine_modules_for_workspace(&self) -> dialoguer::Result<Vec<ModuleToCreate>> {
+        let mut members = Vec::new();
+
+        loop {
+            let add_module = Confirm::with_theme(&self.theme)
+                .with_prompt("Add a module (crate)?")
+                .default(true)
+                .interact()?;
+
+            if !add_module {
+                return Ok(members);
+            }
+
+            let name: String = Input::with_theme(&self.theme)
+                .with_prompt("Name:")
+                .interact_text()?;
+
+            let module_type_selection = Select::with_theme(&self.theme)
+                .with_prompt("Type:")
+                .default(0)
+                .item("Library")
+                .item("Binary")
+                .interact()?;
+            let module_type = match module_type_selection {
+                0 => ModuleType::Lib,
+                1 => ModuleType::Bin,
+                _ => ModuleType::Lib,
+            };
+
+            members.push(ModuleToCreate { name, module_type });
+        }
+    }
+}
